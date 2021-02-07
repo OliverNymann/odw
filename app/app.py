817ee4d7b01a356b6dd0ee19b4630413cc2f96ff
gg <ghostRider>
2021-02-07 20:27:38 +0100

    fixed it

--- app/app.py
+++ app/app.py
@@ -7,9 +7,7 @@ app = Flask(__name__)
 
 config = {
     'user': 'root',
-    #'password': '',
     'host': 'mysql',
-   # 'port': '3307',
     'database': 'odwta'
     }
 
@@ -25,14 +23,14 @@ def postPerson():
         sql = "INSERT INTO persons (Firstname, Lastname) VALUES (%s, %s)";
         val = (request.form['firstname'], request.form['lastname'])
         #val = (request.form.get('firstname'), request.form.get('lastname'))
-        #val = ("John", "Highway 21")
+        #val = ("John", "Highway")
         mycursor.execute(sql, val);
-
-        mysql.commit();
-        connection.commit();
-        mycursor.commit();
+        #mycursor.execute(sql)
 
         print(mycursor.rowcount, "record inserted.")
+
+        connection.commit()
+        mycursor.close()
        
         return Response(status=200)
 
@@ -46,9 +44,10 @@ def home():
 
         myresult = mycursor.fetchall()
 
-        
-        return jsonify(myresult)
-
+        persons = []
+        for row in myresult:
+            persons.append({'PersonID': row[0], 'Firstname': row[1], 'Lastname':row[2]})
+        return json.dumps(persons) 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
