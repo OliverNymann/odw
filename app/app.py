f29bec0a1660bc4613bc4013d9fc6fa11584c380
gg <ghostRider>
2021-02-07 19:13:04 +0100

    rip

--- app/app.py
+++ app/app.py
@@ -7,48 +7,49 @@ app = Flask(__name__)
 
 config = {
     'user': 'root',
-    'password': '',
+    #'password': '',
     'host': 'mysql',
-    'port': '3307',
+   # 'port': '3307',
     'database': 'odwta'
     }
 
 
-#def connectionToDB():
-#    return connection
-#def getDatabaseConnection():
-#    return mysql.connector.connect(user="root", host="mysql", port="3307", password="", database="odwta")
 
-@app.route('/person', methods=['GET', 'POST'])
+@app.route('/person', methods=['POST'])
+def postPerson():
+
+        connection = mysql.connector.connect(**config)
+        
+        mycursor = connection.cursor()
+
+        sql = "INSERT INTO persons (Firstname, Lastname) VALUES (%s, %s)";
+        val = (request.form['firstname'], request.form['lastname'])
+        #val = (request.form.get('firstname'), request.form.get('lastname'))
+        #val = ("John", "Highway 21")
+        mycursor.execute(sql, val);
+
+        mysql.commit();
+        connection.commit();
+        mycursor.commit();
+
+        print(mycursor.rowcount, "record inserted.")
+       
+        return Response(status=200)
+
+@app.route('/persons', methods=['GET', 'POST'])
 def home():
         connection = mysql.connector.connect(**config)
-        #sqlstr = "INSERT INTO persons(firstname, lastname) VALUES (%s %s)", (request.form.get('firstname'), request.form.get('lastname'))
-        #cur = db.cursor()
-        #cur.execute(sqlstr)
-        #db.commit()
+        
         mycursor = connection.cursor()
 
         mycursor.execute("SELECT * FROM persons")
 
         myresult = mycursor.fetchall()
 
-        #return Response(status=200)
+        
         return jsonify(myresult)
-#mysql = MySQL(app)
 
-#@app.route('/person', methods=['GET' , 'POST'])
-#def home():
- #   if request.method == "POST":
-  #      details = request.form
-   #     firstname = details['firstname']
-    #    lastname = details['lastname']
-     #   cur = mysql.connection.cursor()
-      #  cur.execute("INSERT INTO persons(Firstname, Lastname) VALUES (%s %s)", (firstname, lastname))
-       # mysql.connection.commit()
-        #cur.close()
-        #return 'success'
-    #return render_template('insert.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=3000)
 
